2bd9ea6307c0c80adc5999502e1d8aa4ce62e5f1
Divyansh Joshi <divyanshjoshi20@gmail.com>
2021-08-02 17:05:30 +0530

    Added a Cart Provider class

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import './screens/products_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products_provider.dart';
+import './providers/cart.dart';
 
 void main() {
   runApp(ShopApp());
@@ -14,16 +15,22 @@ class ShopApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // This provider widget is added to main.dart because we want this widget to be attached to the topmost widget in the tree
 
-    // Following is the most commonly used provider, it allows us to listen to a class, and whenever that class updates, it rebuilds only those widgets which were listening to the data.
+    return MultiProvider(
+      // Following is the most commonly used provider, it allows us to listen to a class, and whenever that class updates, it rebuilds only those widgets which were listening to the data.
+      // .value approach isn't good here since we're creating a BRAND new instance of this Products() class
+      providers: [
+        //create: instead of builder: in provider v5.0
 
-    // .value approach isn't good here since we're creating a BRAND new instance of this Products() class
-    return ChangeNotifierProvider(
-      //create: instead of builder: in provider v5.0
+        /* This create:/builder: method below basically provides the SAME instance to all the widget below this wiget tree
+          which means the same Object of the 'Providers' class, through which we can acces the same 'item' list  */
 
-      /* This create:/builder: method below basically provides the SAME instance to all the widget below this wiget tree
-        which means the same Object of the 'Providers' class, through which we can acces the same 'item' list  */
-
-      create: (ctx) => Products(),
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Shop App',
         theme: ThemeData(
